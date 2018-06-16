33642167f7f49a57a069456df780f85c9db5d12e
helenosheaa <helenosheaa@gmail.com>
2018-06-16 11:10:44 +0100

    sql tables connected, db created

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+require_relative( "../models/booking.rb" )
+require_relative( "../models/instructor.rb" )
+require_relative( "../models/yogaclass.rb" )
+require("pry-byebug")
--- db/yoga_studio.sql
+++ db/yoga_studio.sql
@@ -1,1 +1,35 @@
+DROP TABLE bookings;
+DROP TABLE members;
+DROP TABLE yogaclasses;
+DROP TABLE instructors;
 
+CREATE TABLE instructors
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255),
+  speciality VARCHAR(255)
+);
+
+CREATE TABLE yogaclasses
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255),
+  time VARCHAR(255),
+  duration VARCHAR(255),
+  level VARCHAR(255),
+  instructor_id INT8 references instructors(id)
+);
+
+CREATE TABLE members
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255),
+  level VARCHAR(255)
+);
+
+CREATE TABLE bookings
+(
+  id SERIAL8 primary key,
+  member_id INT8 references members(id),
+  yogaclass_id INT8 references yogaclasses(id)
+);
